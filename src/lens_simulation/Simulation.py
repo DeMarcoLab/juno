63dc942d3c378696e887bb44219648827b2bcb63
David Dierickx <David.Dierickx1@monash.edu>
2022-02-15 16:08:53 +1100

    feat: calculate_equivalent_focal_distance function

--- src/lens_simulation/Simulation.py
+++ src/lens_simulation/Simulation.py
@@ -1,6 +1,8 @@
 import numpy as np
 from scipy import fftpack
 
+from lens_simulation.Lens import Lens, Medium
+
 
 class Simulation:
     pass
@@ -22,3 +24,29 @@ def generate_squared_frequency_array(n_pixels: int, pixel_size: float) -> np.nda
         squared frequency array
     """
     return np.power(fftpack.fftfreq(n_pixels, pixel_size), 2)
+
+
+def calculate_equivalent_focal_distance(lens: Lens, medium: Medium) -> float:
+    """Calculates the focal distance of a lens with any exponent as if it had
+    an exponent of 2.0 (assuming plano-concave focusing lens)
+
+    Parameters
+    ----------
+    lens : Lens
+        Lens instance as defined in Lens.py
+    medium : Medium
+        Medium instance as defined in Lens.py
+
+    Returns
+    -------
+    float
+        returns the calculated focal distance
+    """
+    equivalent_radius_of_curvature = 0.5 * (
+        lens.height + (lens.diameter / 2) ** 2 / lens.height
+    )
+    equivalent_focal_distance = (
+        equivalent_radius_of_curvature * medium.refractive_index
+    ) / (lens.medium.refractive_index - medium.refractive_index)
+
+    return equivalent_focal_distance
--- tests/test_Simulation.py
+++ tests/test_Simulation.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pytest
-from lens_simulation.Simulation import generate_squared_frequency_array
+from lens_simulation import Simulation, Lens
 
 
 @pytest.mark.parametrize(
@@ -10,7 +10,9 @@ from lens_simulation.Simulation import generate_squared_frequency_array
 def test_generate_squared_frequency_array_even(pixel_size, expected):
     array = np.array(np.ones(4))
     n_pixels = len(array)
-    frequency_array_new = generate_squared_frequency_array(n_pixels, pixel_size)
+    frequency_array_new = Simulation.generate_squared_frequency_array(
+        n_pixels, pixel_size
+    )
 
     assert len(frequency_array_new) == len(array)
     assert np.allclose(frequency_array_new, expected)
@@ -23,8 +25,37 @@ def test_generate_squared_frequency_array_even(pixel_size, expected):
 def test_generate_squared_frequency_array_odd(pixel_size, expected):
     array = np.array(np.ones(5))
     n_pixels = len(array)
-    frequency_array_new = generate_squared_frequency_array(n_pixels, pixel_size)
+    frequency_array_new = Simulation.generate_squared_frequency_array(
+        n_pixels, pixel_size
+    )
 
     assert len(frequency_array_new) == len(array)
     assert np.allclose(frequency_array_new, expected)
 
+
+def test_calculate_equivalent_focal_distance_large():
+    medium = Lens.Medium(1.0)
+    lens = Lens.Lens(200, 20, 2.0, Lens.Medium(1.5))
+
+    focal_distance = Simulation.calculate_equivalent_focal_distance(lens, medium)
+    assert np.isclose(focal_distance, 520, rtol=1e-6)
+
+
+@pytest.mark.parametrize("lens_exponent", [1.0, 1.5, 2.0, 2.1])
+def test_calculate_equivalent_focal_distance(lens_exponent):
+    # all exponents should result in equivalent focal distance
+    medium = Lens.Medium(1.0)
+    lens = Lens.Lens(4500e-6, 70e-6, lens_exponent, Lens.Medium(2.348))
+
+    focal_distance = Simulation.calculate_equivalent_focal_distance(lens, medium)
+    assert np.isclose(focal_distance, 0.0268514, rtol=1e-6)
+
+
+@pytest.mark.parametrize("lens_exponent", [1.0, 1.5, 2.0, 2.1])
+def test_calculate_equivalent_focal_distance_fail_due_to_height(lens_exponent):
+    # changing height changes equivalent focal distance for all exponents
+    medium = Lens.Medium(1.0)
+    lens = Lens.Lens(4500e-6, 80e-6, lens_exponent, Lens.Medium(2.348))
+
+    focal_distance = Simulation.calculate_equivalent_focal_distance(lens, medium)
+    assert not np.isclose(focal_distance, 0.0268514, rtol=1e-6)
