d18d40ed431ea7906dcae2cc5fb4de52f168b583
Patrick Cleeve <patrick.cleeve@monash.edu>
2022-03-31 13:05:58 +1100

    working simulation function

--- src/lens_simulation/Simulation.py
+++ src/lens_simulation/Simulation.py
@@ -48,41 +48,36 @@ class Simulation:
 
         print("HELLO")
 
-
         # lens creation
         amplitude = 10000
         sim_width = 4500e-6
-        pixel_size = 1e-6 
+        pixel_size = 1e-6
         n_slices = 1000
 
         lens_1 = Lens.Lens(
-            diameter=sim_width,
-            height=70e-6,
-            exponent=0.,
-            medium=Lens.Medium(2.348)
+            diameter=sim_width, height=70e-6, exponent=0.0, medium=Lens.Medium(2.348)
         )
         lens_1.generate_profile(pixel_size=pixel_size)
 
-        centre_px = (len(lens_1.profile)-1)//2
+        centre_px = (len(lens_1.profile) - 1) // 2
 
+        plt.title("Lens 1 Profile")
         plt.plot(lens_1.profile)
         plt.show()
 
         # lens 2 creation
         amplitude = 10000
         sim_width = 4500e-6
-        pixel_size = 1e-6 
-
+        pixel_size = 1e-6
+        self.pixel_size = pixel_size
         lens_2 = Lens.Lens(
-            diameter=sim_width,
-            height=70e-6,
-            exponent=2.,
-            medium=Lens.Medium(2.348)
+            diameter=sim_width, height=70e-6, exponent=2.0, medium=Lens.Medium(2.348)
         )
         lens_2.generate_profile(pixel_size=pixel_size)
 
-        centre_px = (len(lens_2.profile)-1)//2
+        centre_px = (len(lens_2.profile) - 1) // 2
 
+        plt.title("Lens 2 Profile")
         plt.plot(lens_2.profile)
         plt.show()
 
@@ -93,175 +88,135 @@ class Simulation:
         sim_wavelength = 488e-9
         step_size = 0.1e-6
 
-
-
-        # Simulation Calculations
-        # lens 1 
-
-
-
-        def propagate_wavefront(lens, output_medium, n_slices, start_distance, finish_distance):
-            print("Lens 1")
-            frequency_array_1 = generate_squared_frequency_array(
-                n_pixels=len(lens.profile), pixel_size=pixel_size)
-
-            print(len(frequency_array_1))
-
-            delta_1 = (lens.medium.refractive_index-output_medium.refractive_index) * lens.profile
-            phase_1 = (2 * np.pi * delta_1 / sim_wavelength) % (2 * np.pi)
-            wavefront_1 = A * np.exp(1j * phase_1)
-            wavefront_1 = fftpack.fft(wavefront_1)
-
-            start_distance = 0.
-            finish_distance = 10.e-3
-
-
-            sim = np.ones(shape=((n_slices), len(lens.profile)))
-            distances_1 = np.linspace(start_distance, finish_distance, n_slices)
-            for i, z in enumerate(distances_1):
-                prop_1 = np.exp(1j * output_medium.wave_number * z) * np.exp(
-                    (-1j * 2 * np.pi ** 2 * z * frequency_array_1) / output_medium.wave_number
-                )
-                # print("prop shape: ", prop.shape)
-                propagation = fftpack.ifft(prop_1 * wavefront_1)
-
-                output = np.sqrt(propagation.real ** 2 + propagation.imag ** 2)
-
-                sim[i] = np.round(output, 10)
-            from lens_simulation import utils
-            utils.plot_simulation(sim, sim.shape[1], sim.shape[0], 
-                                    pixel_size, start_distance, finish_distance)
-            # plt.imshow(sim)
-            plt.show()
-
-            return propagation
+        self.A = A
+        self.pixel_size = pixel_size
+        self.sim_wavelength = sim_wavelength
         
-        propagation = propagate_wavefront(lens=lens_1, output_medium=output_medium_1, 
-                    n_slices=100, start_distance=0., finish_distance=10.e-3)
+        
+        # Simulation Calculations
+        # lens 1
+        propagation = self.propagate_wavefront(
+            lens=lens_1,
+            output_medium=output_medium_1,
+            n_slices=100,
+            start_distance=0.0,
+            finish_distance=10.0e-3,
+        )
 
         passed_wavefront = propagation
 
         # lens 2
         print("Lens 2")
-        frequency_array_2 = generate_squared_frequency_array(
-            n_pixels=len(lens_2.profile), pixel_size=pixel_size)
+        equivalent_focal_distance_2 = calculate_equivalent_focal_distance(
+            lens_2, output_medium_2
+        )
+        start_distance = 0 * equivalent_focal_distance_2
+        finish_distance = 2 * equivalent_focal_distance_2
 
-        print(len(frequency_array_2))
-
-        delta_2 = (lens_2.medium.refractive_index-output_medium_2.refractive_index) * lens_2.profile
-        phase_2 = (2 * np.pi * delta_2 / sim_wavelength) % (2 * np.pi)
-        wavefront_2 = A * np.exp(1j * phase_2) * passed_wavefront
-        fft_wavefront_2 = fftpack.fft(wavefront_2)
-
-        equivalent_focal_distance_2 = calculate_equivalent_focal_distance(lens_2, output_medium_2)
-        start_distance_2 = 0 * equivalent_focal_distance_2
-        finish_distance_2 = 2 * equivalent_focal_distance_2
-
-        n_slices_2 = 1000
-
-        sim_2 = np.ones(shape=((n_slices_2), len(lens_2.profile)))
-        distances_2 = np.linspace(start_distance_2, finish_distance_2, n_slices_2)
-        for i, z in enumerate(distances_2):
-            prop_2 = np.exp(1j * output_medium_2.wave_number * z) * np.exp(
-                (-1j * 2 * np.pi ** 2 * z * frequency_array_2) / output_medium_2.wave_number
-            )
-            # print("prop shape: ", prop.shape)
-            propagation = fftpack.ifft(prop_2 * fft_wavefront_2)
-
-            output = np.sqrt(propagation.real ** 2 + propagation.imag ** 2)
-
-            sim_2[i] = np.round(output, 10)
+        propagation = self.propagate_wavefront(
+            lens=lens_2,
+            output_medium=output_medium_2,
+            n_slices=1000,
+            start_distance=start_distance,
+            finish_distance=finish_distance,
+            passed_wavefront=passed_wavefront,
+        )
 
         passed_wavefront = propagation
-
-        from lens_simulation import utils
-        utils.plot_simulation(sim_2, sim_2.shape[1], sim_2.shape[0], pixel_size, start_distance_2, finish_distance_2)
-        # plt.imshow(sim)
-        plt.show()
 
         # output 2
         print("Output 2")
-        frequency_array_2 = generate_squared_frequency_array(
-            n_pixels=len(lens_2.profile), pixel_size=pixel_size)
 
-        print(len(frequency_array_2))
-
-        delta_2 = (lens_2.medium.refractive_index-output_medium_2.refractive_index) * lens_2.profile
-        phase_2 = (2 * np.pi * delta_2 / sim_wavelength) % (2 * np.pi)
-        wavefront_2 = A * np.exp(1j * phase_2) * passed_wavefront
-        fft_wavefront_2 = fftpack.fft(wavefront_2)
-
-        equivalent_focal_distance_2 = calculate_equivalent_focal_distance(lens_2, output_medium_2)
-        start_distance_2 = 0 * equivalent_focal_distance_2
-        finish_distance_2 = 2 * equivalent_focal_distance_2
-
-        n_slices_2 = 1000
-
-        sim_2 = np.ones(shape=((n_slices_2), len(lens_2.profile)))
-        distances_2 = np.linspace(start_distance_2, finish_distance_2, n_slices_2)
-        for i, z in enumerate(distances_2):
-            prop_2 = np.exp(1j * output_medium_2.wave_number * z) * np.exp(
-                (-1j * 2 * np.pi ** 2 * z * frequency_array_2) / output_medium_2.wave_number
-            )
-            # print("prop shape: ", prop.shape)
-            propagation = fftpack.ifft(prop_2 * fft_wavefront_2)
-
-            output = np.sqrt(propagation.real ** 2 + propagation.imag ** 2)
-
-            sim_2[i] = np.round(output, 10)
+        propagation = self.propagate_wavefront(
+            lens=lens_2,
+            output_medium=output_medium_2,
+            n_slices=1000,
+            start_distance=start_distance,
+            finish_distance=finish_distance,
+            passed_wavefront=passed_wavefront,
+        )
 
         passed_wavefront = propagation
 
-        utils.plot_simulation(sim_2, sim_2.shape[1], sim_2.shape[0], pixel_size, start_distance_2, finish_distance_2)
-        # plt.imshow(sim)
-        plt.show()
-
-        # lens 3
+        # # lens 3
         print("Lens 3")
-        frequency_array_2 = generate_squared_frequency_array(
-            n_pixels=len(lens_2.profile), pixel_size=pixel_size)
-
-        print(len(frequency_array_2))
-
-        delta_2 = (lens_2.medium.refractive_index-output_medium_2.refractive_index) * lens_2.profile
-        phase_2 = (2 * np.pi * delta_2 / sim_wavelength) % (2 * np.pi)
-        wavefront_2 = A * np.exp(1j * phase_2) * passed_wavefront
-        fft_wavefront_2 = fftpack.fft(wavefront_2)
-
-        equivalent_focal_distance_2 = calculate_equivalent_focal_distance(lens_2, output_medium_2)
-        start_distance_2 = 0 * equivalent_focal_distance_2
-        finish_distance_2 = 2 * equivalent_focal_distance_2
-
-        n_slices_2 = 1000
-
-        sim_2 = np.ones(shape=((n_slices_2), len(lens_2.profile)))
-        distances_2 = np.linspace(start_distance_2, finish_distance_2, n_slices_2)
-        for i, z in enumerate(distances_2):
-            prop_2 = np.exp(1j * output_medium_2.wave_number * z) * np.exp(
-                (-1j * 2 * np.pi ** 2 * z * frequency_array_2) / output_medium_2.wave_number
-            )
-            # print("prop shape: ", prop.shape)
-            propagation = fftpack.ifft(prop_2 * fft_wavefront_2)
-
-            output = np.sqrt(propagation.real ** 2 + propagation.imag ** 2)
-
-            sim_2[i] = np.round(output, 10)
+        propagation = self.propagate_wavefront(
+            lens=lens_2,
+            output_medium=output_medium_2,
+            n_slices=1000,
+            start_distance=start_distance,
+            finish_distance=finish_distance,
+            passed_wavefront=passed_wavefront,
+        )
 
         passed_wavefront = propagation
-
-        utils.plot_simulation(sim_2, sim_2.shape[1], sim_2.shape[0], pixel_size, start_distance_2, finish_distance_2)
-        # plt.imshow(sim)
-        plt.show()
-
-
-
-
-
-
-
-
 
         print("-" * 50)
+
+    def propagate_wavefront(
+        self,
+        lens,
+        output_medium,
+        n_slices,
+        start_distance,
+        finish_distance,
+        passed_wavefront=None,
+    ):
+
+        # TODO: docstring
+        # TODO: input validation
+        print("-" * 20)
+        print(f"Propagating Wavefront with Parameters")
+        print(f"Lens: {lens}")
+        print(f"Medium: {output_medium}")
+        print(
+            f"Slices: {n_slices}, Start: {start_distance:.2e}m, Finish: {finish_distance:.2e}m"
+        )
+        print(f"Passed Wavefront: {passed_wavefront is not None}")
+        print(f"-" * 20)
+
+        freq_arr = generate_squared_frequency_array(
+            n_pixels=len(lens.profile), pixel_size=self.pixel_size
+        )
+
+        delta = (
+            lens.medium.refractive_index - output_medium.refractive_index
+        ) * lens.profile
+        phase = (2 * np.pi * delta / self.sim_wavelength) % (2 * np.pi)
+        if passed_wavefront is not None:
+            wavefront = self.A * np.exp(1j * phase) * passed_wavefront
+        else:
+            wavefront = self.A * np.exp(1j * phase)
+
+        fft_wavefront = fftpack.fft(wavefront)
+
+        sim = np.ones(shape=((n_slices), len(lens.profile)))
+        distances_2 = np.linspace(start_distance, finish_distance, n_slices)
+        for i, z in enumerate(distances_2):
+            prop = np.exp(1j * output_medium.wave_number * z) * np.exp(
+                (-1j * 2 * np.pi ** 2 * z * freq_arr) / output_medium.wave_number
+            )
+            propagation = fftpack.ifft(prop * fft_wavefront)
+
+            output = np.sqrt(propagation.real ** 2 + propagation.imag ** 2)
+
+            sim[i] = np.round(output, 10)
+
+        from lens_simulation import utils
+
+        # plot sim result
+        utils.plot_simulation(
+            sim,
+            sim.shape[1],
+            sim.shape[0],
+            self.pixel_size,
+            start_distance,
+            finish_distance,
+        )
+        # plt.imshow(sim)
+        plt.show()
+
+        return propagation
 
 
 def generate_squared_frequency_array(n_pixels: int, pixel_size: float) -> np.ndarray:
