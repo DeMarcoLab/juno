0c548f5fa26ff4e8ed02bd4d9fc6d39d8a0b08e5
DavidDierickx <David.Dierickx1@monash.edu>
2023-11-27 21:58:38 +1100

    base herschel structures + eq refactor

---
+++ juno/juno_custom/elements/Herschel/structures.py
@@ -0,0 +1,74 @@
+from dataclasses import dataclass
+
+import numpy as np
+
+@dataclass
+class HerschelSettings:
+    """Class to hold the settings for generating the Herschel element."""
+    radius: float = 9 # radius of the first lens surface
+    radius_buffer: float = 0.01 # starting point for calculations of first lens surface
+    thickness: float = 15 # nominal thickness of the lens
+    exponent: float = 2 # initial guess of the exponent of the lens
+    z_medium_o: float = -30 # position of the design point source relative to the first lens surface
+    z_medium_i: float = 60 # position of the design final image point relative to the second lens surface
+    n_medium_o: float = 1 # refractive index of the medium before the first lens surface
+    n_medium_i: float = 1 # refractive index of the medium after the second lens surface
+    n_lens: float = 2.  # refractive index of the lens
+    magnification: float = 2  # axial magnification of the lens
+    a: float = 0 # initial guess for the root finding method
+    tolerance: float = 1 # tolerance value to evaluate success of root finding method
+    max_iter: int = 2000 # maximum number of iterations for the root finding method
+    
+    radii: np.ndarray = None # array of radii of first lens surface
+
+
+    @staticmethod
+    def from_dict(settings):
+        return HerschelSettings(
+            radius=settings["radius"],
+            radius_buffer=settings["radius_buffer"],
+            thickness=settings["thickness"],
+            exponent=settings["exponent"],
+            z_medium_o=settings["z_medium_o"],
+            z_medium_i=settings["z_medium_i"],
+            n_medium_o=settings["n_medium_o"],
+            n_medium_i=settings["n_medium_i"],
+            n_lens=settings["n_lens"],
+            magnification=settings["magnification"],
+            a=settings["a"],
+            tolerance=settings["tolerance"],
+            max_iter=settings["max_iter"] if "max_iter" in settings else 200000,
+        )
+    
+    def to_dict(settings):
+        return {
+            "radius": settings.radius,
+            "radius_buffer": settings.radius_buffer,
+            "thickness": settings.thickness,
+            "exponent": settings.exponent,
+            "z_medium_o": settings.z_medium_o,
+            "z_medium_i": settings.z_medium_i,
+            "n_medium_o": settings.n_medium_o,
+            "n_medium_i": settings.n_medium_i,
+            "n_lens": settings.n_lens,
+            "magnification": settings.magnification,
+            "a": settings.a,
+            "tolerance": settings.tolerance,
+            "max_iter": settings.max_iter,    
+        }
+
+    def calculate_radii(self, pixel_size):
+        self.radii = np.linspace(
+            self.radius_buffer,
+            self.radius,
+            int((self.radius - self.radius_buffer)/ pixel_size) + 1,
+        )
+
+@dataclass
+class HerschelResults:
+    """Class to hold the results of generating the Herschel element."""
+    lens_roots: np.ndarray = None  # the roots of the lens surface
+    lens_x_first: np.ndarray = None  # the radial points of the first lens surface
+    lens_y_first: np.ndarray = None  # the height values of the first lens surface
+    lens_x_second: np.ndarray = None  # the radial points of the second lens surface
+    lens_y_second: np.ndarray = None  # the height values of the second lens surface
